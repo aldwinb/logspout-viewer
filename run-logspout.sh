3dacd238e987ce3aa2b7027d7b4f87a08f691764
Aldwin Barredo <aldwinb@users.noreply.github.com>
2016-05-11 11:13:44 -0400

    Don't start a logspout container named logspout-1 if logspout is already running. Added 5 sec delay before displying logs so that curl won't fail.

--- run-logspout.sh
+++ run-logspout.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-docker run \
+if [ "$(docker ps -f name=logspout-1 --format "{{.Names}}" | wc -l)" -eq "0" ] ; then
+  echo "Creating logspout-1"
+  docker run \
   --name logspout-1 \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -p 9001:80 \
   -d gliderlabs/logspout:latest \
   syslog://logs.papertrailapp.com:55555 1> /dev/null
 
+  sleep 5
+else
+  echo "Already running a logspout-1 container"
+fi
+
+echo "Displaying logs..."
 curl http://localhost:9001/logs
